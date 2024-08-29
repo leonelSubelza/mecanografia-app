67e6661b783d39b40805b0c422e330f615378689
Leonel Subelza <subelza150@gmail.com>
2024-08-29 17:15:47 -0300

    The size of the texts are changed

--- src/app/shared/mock/texts.mock.ts
+++ src/app/shared/mock/texts.mock.ts
@@ -3,7 +3,7 @@ import { TextContent } from '../../interfaces/entities';
 export const texts: TextContent[] = [
   {
     title: 'Red Dead Redemption',
-    text: 'Red Dead Redemption 2 es un videojuego de acción-aventura de mundo abierto desarrollado y publicado por Rockstar Games. El juego es la tercera entrada de la serie Red Dead y una precuela del juego de 2010 Red Dead Redemption. La historia está ambientada en una representación ficticia de los Estados Unidos en 1899 y sigue las hazañas de Arthur Morgan, un forajido y miembro de la pandilla de Dutch Van der Linde, que debe lidiar con el declive del Salvaje Oeste mientras intenta sobrevivir contra las fuerzas gubernamentales, las pandillas rivales y otros adversarios.'
+    text: 'La historia está ambientada en una representación ficticia de los Estados Unidos en 1899 y sigue las hazañas de Arthur Morgan, un forajido y miembro de la pandilla de Dutch Van der Linde, que debe lidiar con el declive del Salvaje Oeste mientras intenta sobrevivir contra las fuerzas gubernamentales, las pandillas rivales y otros adversarios.'
   },
   {
     title: 'Hollow Knight',
@@ -15,19 +15,19 @@ export const texts: TextContent[] = [
   },
   {
     title: 'Geralt of Rivia',
-    text: 'Geralt de Rivia, el brujo, se gana la vida como cazador de monstruosas criaturas en una tierra de magia y maravilla: con sus dos espadas al hombro -la de acero y la de plata- da cuenta de estriges, mantícoras, grifos, vampiros, quimeras y lobisones, pero solo cuando amenazan la paz. Sarcástico, cínico, descreído y siempre errante, sus pasos le llevan de pueblo en pueblo ofreciendo sus servicios, hallando las más de las veces que los auténticos monstruos se esconden bajo rostros humanos.'
+    text: 'Geralt de Rivia, el brujo, se gana la vida como cazador de monstruosas criaturas en una tierra de magia y maravilla: con sus dos espadas al hombro -la de acero y la de plata- da cuenta de estriges, mantícoras, grifos, vampiros, quimeras y lobisones, pero solo cuando amenazan la paz.'
   },
   {
     title: 'Programación reactiva',
-    text: 'La programación reactiva en Angular es un enfoque de desarrollo que se basa en el patrón de programación reactiva y utiliza la biblioteca RxJS (Reactive Extensions for JavaScript) para manejar flujos de datos asíncronos y eventos en una aplicación. En la programación reactiva, los componentes de una aplicación son vistos como productores y consumidores de eventos o flujos de datos.'
+    text: 'La programación reactiva en Angular es un enfoque de desarrollo que se basa en el patrón de programación reactiva y utiliza la biblioteca RxJS (Reactive Extensions for JavaScript) para manejar flujos de datos asíncronos y eventos en una aplicación.'
   },
   {
     title: 'Polimorfismo',
-    text: 'En programación orientada a objetos, el polimorfismo se refiere a la propiedad por la que es posible enviar mensajes sintácticamente iguales a objetos de tipos distintos. Esta característica permite que, sin alterar ni tocar el código existente, se puedan incorporar nuevos comportamientos y funciones (es decir la interfaz sintáctica se mantiene inalterada pero cambia el comportamiento en función de qué objeto estamos usando en cada momento).'
+    text: 'En programación orientada a objetos, el polimorfismo se refiere a la propiedad por la que es posible enviar mensajes sintácticamente iguales a objetos de tipos distintos.'
   },
   {
     title: 'Herencia',
-    text: 'La herencia es uno de los mecanismos de los lenguajes de programación orientada a objetos basados en clases, por medio del cual una clase se deriva de otra de manera que extiende su funcionalidad. La clase de la que se hereda se suele denominar clase base, clase padre, superclase, clase ancestro (el vocabulario que se utiliza suele depender en gran medida del lenguaje de programación).'
+    text: 'La herencia es uno de los mecanismos de los lenguajes de programación orientada a objetos basados en clases, por medio del cual una clase se deriva de otra de manera que extiende su funcionalidad.'
   },
 
 ];
